4072fa4f55dbfd7a2dff1fafc3e2a060d11d0e26
Gustavo Soares <gsoares@dsc.ufcg.edu.br>
2016-10-05 13:39:31 -0700

    adding missing models

---
+++ Refazer.WebAPI/Models/Session.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Refazer.WebAPI.Models
+{
+    /// <summary>
+    /// This class represents an experiement session in a user study for evaluation
+    /// our grading ui
+    /// </summary>
+    public class Session
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        //TODO: add more information about the experiment? 
+    }
+
+    public class SessionDbContext : DbContext
+    {
+        public DbSet<Session> Sessions { set; get; }
+    }
+}
