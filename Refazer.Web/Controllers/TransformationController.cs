2939b095951b0fa25fd0fb251ef00bb7368b97b4
Ruan Reis <ruanvictorreis@gmail.com>
2017-08-02 15:42:18 -0300

    Saving a transformation in database

---
+++ Refazer.Web/Controllers/TransformationController.cs
@@ -0,0 +1,24 @@
+﻿using Refazer.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Refazer.Web.Controllers
+{
+    [RoutePrefix("api/transformations")]
+    public class TransformationController : ApiController
+    {
+
+        private RefazerDbContext db = new RefazerDbContext();
+
+        // GET api/<controller>
+        [Route("")]
+        public IQueryable<Transformation2> GetTransformations()
+        {
+            return db.Transformations2;
+        }
+    }
+}
