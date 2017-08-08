bc5ea32be15b236020d62486d21bb78440473b52
Ruan Reis <ruanvictorreis@gmail.com>
2017-08-08 16:47:00 -0300

    Adding API to fix incorrect submissions

---
+++ Refazer.Web/Models/Submission2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Refazer.WebAPI.Models
+{
+    public class Submission2
+    {
+        [Required]
+        public String Question { get; set; }
+
+        [Required]
+        public String IncorrectCode { get; set; }
+
+        [Required]
+        public String EndPoint { get; set; }
+
+        [ForeignKey("EndPoint")]
+        public Assignment Assignment { get; set; }
+    }
+}
